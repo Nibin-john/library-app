bc54c65b71470f14f3f5ddca34db40e2fa7d3336
Nibin-john <nibinjohn24@gmail.com>
2022-07-07 16:55:49 +0530

    libraryapp completed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SearchBookComponent } from './search-book/search-book.component';
+import { AddBookComponent } from './add-book/add-book.component';
+import { ViewBookComponent } from './view-book/view-book.component';
+import { DeleteBookComponent } from './delete-book/delete-book.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SearchBookComponent,
+    AddBookComponent,
+    ViewBookComponent,
+    DeleteBookComponent
   ],
   imports: [
     BrowserModule,
